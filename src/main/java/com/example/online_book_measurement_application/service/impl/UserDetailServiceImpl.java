ac1121d110d32421e0e23592cd6bc276d5607351
Minh Phuc <lephuc04b3@gmail.com>
2025-03-03 09:29:30 +0700

    feat: add UserDetailServiceImpl

---
+++ src/main/java/com/example/online_book_measurement_application/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.online_book_measurement_application.service.impl;
+
+import com.example.online_book_measurement_application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.online_book_measurement_application.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(user.getRole().getName())
+                .build();
+    }
+}
