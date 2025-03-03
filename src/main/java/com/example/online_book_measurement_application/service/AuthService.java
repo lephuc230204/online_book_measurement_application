9da794c2f25ff86cadbfad5eb161266e6e8966da
Minh Phuc <lephuc04b3@gmail.com>
2025-03-03 20:40:18 +0700

    refactor: update method confirm, login

--- src/main/java/com/example/online_book_measurement_application/service/AuthService.java
+++ src/main/java/com/example/online_book_measurement_application/service/AuthService.java
@@ -1,9 +1,15 @@
 package com.example.online_book_measurement_application.service;
 
 import com.example.online_book_measurement_application.dto.request.SignInRequest;
+import com.example.online_book_measurement_application.dto.request.SignUpRequest;
 import com.example.online_book_measurement_application.dto.response.AuthDto;
 import com.example.online_book_measurement_application.dto.response.ResponseData;
 
 public interface AuthService {
-    ResponseData<AuthDto> logib(SignInRequest signInRequest);
+    ResponseData<String> register(SignUpRequest signUpRequest);
+
+
+    //    ResponseData<String> logout(HttpServletRequest request, HttpServletResponse response);
+    ResponseData<String> confirmUser(long userId, String otpCode);
+    ResponseData<AuthDto> login(SignInRequest signInRequest);
 }
