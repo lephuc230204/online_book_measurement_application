b4e83312f4a2123e8ea9c6835ec6edcadb9afed6
Minh Phuc <lephuc04b3@gmail.com>
2025-03-03 20:36:09 +0700

    feat: add bean Restemplate

--- src/main/java/com/example/online_book_measurement_application/OnlineBookMeasurementApplication.java
+++ src/main/java/com/example/online_book_measurement_application/OnlineBookMeasurementApplication.java
@@ -2,6 +2,8 @@ package com.example.online_book_measurement_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OnlineBookMeasurementApplication {
@@ -10,4 +12,8 @@ public class OnlineBookMeasurementApplication {
         SpringApplication.run(OnlineBookMeasurementApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
