adeea1be7f1eb80c21387595b69fc21fdbb35bfe
Minh Phuc <lephuc04b3@gmail.com>
2025-03-03 09:27:55 +0700

    init test

---
+++ src/test/java/com/example/online_book_measurement_application/OnlineBookMeasurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_book_measurement_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookMeasurementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
