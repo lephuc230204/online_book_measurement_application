a0e28c3a8ddc59b7a4953b18f280c3689fadd58e
Minh Phuc <lephuc04b3@gmail.com>
2025-03-03 20:38:22 +0700

    refactor: update method static AuthDto builder

--- src/main/java/com/example/online_book_measurement_application/dto/response/AuthDto.java
+++ src/main/java/com/example/online_book_measurement_application/dto/response/AuthDto.java
@@ -1,5 +1,6 @@
 package com.example.online_book_measurement_application.dto.response;
 
+import com.example.online_book_measurement_application.entity.User;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,5 +9,13 @@ import lombok.Data;
 public class AuthDto {
     private Long id;
     private String accessToken;
-    private String userName;
+    private String email;
+
+    public static AuthDto from(User user, String accessToken){
+        return AuthDto.builder()
+                .id(user.getUserId())
+                .accessToken(accessToken)
+                .email(user.getEmail())
+                .build();
+    }
 }
